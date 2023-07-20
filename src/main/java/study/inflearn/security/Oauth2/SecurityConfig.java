0037209809fc856cad40701c7580520e6420e75d
mygoaway <mygoaway2@gmail.com>
2023-07-21 08:51:02 +0900

    'AuthenticationEntryPoint 이해' 강의 후 커밋

--- src/main/java/study/inflearn/security/Oauth2/SecurityConfig.java
+++ src/main/java/study/inflearn/security/Oauth2/SecurityConfig.java
@@ -1,18 +1,32 @@
 package study.inflearn.security.Oauth2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer().setFlag(true));
+        http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("custom entryPoint");
+            }
+        });
+
         return http.build();
     }
 }

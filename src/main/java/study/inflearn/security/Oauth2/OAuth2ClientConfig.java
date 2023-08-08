cfd951a3d6d5d6b9ce1b9e647ef2e5257e6a321e
mygoaway <mygoaway2@gmail.com>
2023-08-09 00:14:42 +0900

    OAuth2LoginConfigurer 관련 강의 후 커밋

--- src/main/java/study/inflearn/security/Oauth2/OAuth2ClientConfig.java
+++ src/main/java/study/inflearn/security/Oauth2/OAuth2ClientConfig.java
@@ -1,26 +1,17 @@
 package study.inflearn.security.Oauth2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 public class OAuth2ClientConfig {
 
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keyCloakClientRegistration());
-    }
-
-    private ClientRegistration keyCloakClientRegistration() {
-        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-                .registrationId("keycloakManual")
-                .clientId("oauth2-client-app")
-                .clientSecret("TCUwfHOUaTMdovquaEOeG8H4pZah7X6q")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        httpSecurity.oauth2Login();
+        return httpSecurity.build();
     }
 }

6b08145940f38791aefdf047ee4c953971b6e4cf
mygoaway <mygoaway2@gmail.com>
2023-08-13 21:11:15 +0900

    OAuth2 로그인 구현 OpenID Connect 로그아웃관련 강의 후 커밋(재수정)

--- src/main/java/study/inflearn/security/Oauth2/OAuth2ClientConfig.java
+++ src/main/java/study/inflearn/security/Oauth2/OAuth2ClientConfig.java
@@ -1,23 +1,45 @@
 package study.inflearn.security.Oauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class OAuth2ClientConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        //httpSecurity.authorizeRequests().anyRequest().authenticated();
-        httpSecurity.authorizeRequests(authRequest -> authRequest
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        //httpSecurity.authorizeRequests(authRequest -> authRequest
                 //.antMatchers("/loginPage")
                 //.permitAll()
-                .anyRequest().permitAll());
+        //        .anyRequest().permitAll());
         httpSecurity.oauth2Login(Customizer.withDefaults());
         //httpSecurity.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
+
+        httpSecurity.logout()
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JESSIONID");
+
         return httpSecurity.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+        return successHandler;
     }
 }

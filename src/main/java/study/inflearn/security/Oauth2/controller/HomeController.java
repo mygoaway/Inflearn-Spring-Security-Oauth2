79c0a2d3115b4bf425dc83119f60eff1c80388da
mygoaway <mygoaway2@gmail.com>
2023-08-22 00:17:53 +0900

    DefaultOAuth2AuthorizedClientManager - Refresh Token 권한 부여 구현하기 강의 후 커밋(누락)

---
+++ src/main/java/study/inflearn/security/Oauth2/controller/HomeController.java
@@ -0,0 +1,12 @@
+package study.inflearn.security.Oauth2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
